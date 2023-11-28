33b410e5170384dfce5bc44748fabe4a8dcb2d31
marwagalal204 <marwagalal204@gmail.com>
2023-11-28 11:06:47 +0200

    fixing from task 0 to task 4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
-#!/usr/bin/pyton3
+#!/usr/bin/python3
 for letter in range(97, 123):
     letter = chr(letter)
     if letter == 'e' or letter == 'q':
         continue
     else:
-        print(letter, end="")
+        print("{}".format(letter), end="")
