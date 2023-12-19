a5d527c0e57b6625fd0f60d3c97103df3283311c
marwagalal204 <marwagalal204@gmail.com>
2023-12-19 20:54:13 +0200

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""a class Square that defines a square"""
 class Square:
+    """define attribute size"""
     def __init__(self, size):
+        """size of a square"""
         self.__size = size
