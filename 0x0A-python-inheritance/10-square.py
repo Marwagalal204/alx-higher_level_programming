bc5de01e79c04c2c45af8bf90f225dd3191c8b4e
marwagalal204 <marwagalal204@gmail.com>
2024-01-08 20:18:00 +0200

    task9

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,11 +3,15 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Squre(Rectangle):
+class Square(Rectangle):
     """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square."""
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,12 +1,12 @@
-"""a Rectangle subclass Square."""
+#!/usr/bin/python3
+""" a class BaseGeometry """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """subclass"""
 
     def __init__(self, size):
-        """Initialize a new square."""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
