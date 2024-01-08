31d6b964016b334c21fc1a928623e56d5182316e
marwagalal204 <marwagalal204@gmail.com>
2024-01-08 19:54:00 +0200

    task2

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Defines a class-checking function."""
+
 
 def is_kind_of_class(obj, a_class):
+    """Check if an object is exactly an instance of a given class."""
     if type(obj) is a_class or isinstance(obj, a_class):
         return True
     else:
-        False
+        return False
