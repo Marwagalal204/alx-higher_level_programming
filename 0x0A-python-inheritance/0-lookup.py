d75f309439a14ecd694eb31243103ca8c8b24d4e
marwagalal204 <marwagalal204@gmail.com>
2024-01-08 07:02:45 +0200

    task0-11

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    attr = dir(lookup)
+    return attr
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    def print_sorted(self):
+        sortedlist = sorted(self)
+        print(sortedlist)
+
