95420092866cef99a8122bfcec5c260cbbc1add4
marwagalal204 <marwagalal204@gmail.com>
2024-01-15 18:30:43 +0200

    base test

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,73 +1,158 @@
 #!/usr/bin/python3
-
+# -*- coding: utf-8 -*-
 """
-unittest for rectangle class
+unittests for rectangle class
 """
-
 import sys
 import unittest
+import inspect
 import io
+import pep8
+from contextlib import redirect_stdout
 from models.rectangle import Rectangle
 
 
-class test_Rectangle(unittest.TestCase):
-    """Testing rectangle class methods"""
+class TestRectangle(unittest.TestCase):
+    """
+    class for testing Rectangle class' methods
+    """
 
-    def setUp(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class method for the doc tests
+        """
+        cls.setup = inspect.getmembers(Rectangle, inspect.isfunction)
 
-    def tearDown(self):
-        pass
+    def test_pep8_conformance_rectangle(self):
+        """
+        Test that rectangle.py file conform to PEP8
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_constructor(self):
-        rect = Rectangle(5, 10, 2, 3, 1)
-        self.assertEqual(rect.width, 5)
-        self.assertEqual(rect.height, 10)
-        self.assertEqual(rect.x, 2)
-        self.assertEqual(rect.y, 3)
-        self.assertEqual(rect.id, 1)
+    def test_module_docstring(self):
+        """
+        Tests if module docstring documentation exist
+        """
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
 
-    def test_width_setter(self):
-        rect = Rectangle(5, 10)
-        rect.width = 15
-        self.assertEqual(rect.width, 15)
+    def test_class_docstring(self):
+        """
+        Tests if class docstring documentation exist
+        """
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
 
-        with self.assertRaises(TypeError):
-            rect.width = "string"
-
+    def test_wrong_inputted_values(self):
+        """
+        Test for negative and zero values
+        """
         with self.assertRaises(ValueError):
-            rect.width = 0
+            R = Rectangle(0, 0)
+        with self.assertRaises(ValueError):
+            R = Rectangle(-4, -5)
+        with self.assertRaises(ValueError):
+            R = Rectangle(1, 1, -2, -2)
+        with self.assertRaises(TypeError):
+            R = Rectangle()
+        with self.assertRaises(TypeError):
+            R = Rectangle(1, 2, 3, 4, 5, 6, 7)
+
+    def test_inputted_types(self):
+        """
+        Different types for inputted arguments
+        """
+        with self.assertRaises(TypeError):
+            R = Rectangle('width', 'height')
+        with self.assertRaises(TypeError):
+            R = Rectangle(2.4, 1.3)
+        with self.assertRaises(TypeError):
+            R = Rectangle(1, 2, 'x value', 'y value')
+        with self.assertRaises(TypeError):
+            R = Rectangle(1, 2, 2.4, 1.3)
+        with self.assertRaises(TypeError):
+            R = Rectangle(True, False)
+        with self.assertRaises(TypeError):
+            R = Rectangle(1, 2, True, False)
+        with self.assertRaises(TypeError):
+            R = Rectangle([1, 1], 2, 3, 4)
+        with self.assertRaises(TypeError):
+            R = Rectangle((1, 2), 'x value', 'y value')
+        with self.assertRaises(TypeError):
+            R = Rectangle({1: 3, 2: 4}, 5, 6)
 
     def test_area(self):
-        rect = Rectangle(5, 10)
-        self.assertEqual(rect.area(), 50)
-
-    def test_display(self):
-        pass
+        """
+        Test for area method
+        """
+        R = Rectangle(10, 10)
+        self.assertEqual(R.area(), 100)
+        with self.assertRaises(TypeError):
+            A = R.area(1)
 
     def test_str(self):
-        rect = Rectangle(5, 10, 2, 3, 1)
-        expected_str = '[Rectangle] (1) 2/3 - 5/10'
-        self.assertEqual(str(rect), expected_str)
+        """
+        Test for __str__ method
+        """
+        R = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual("[Rectangle] (5) 3/4 - 1/2", str(R))
 
-    def test_update(self):
-        rect = Rectangle(5, 10, 2, 3, 1)
-        rect.update(2, 8, 12, 4, 5)
-        self.assertEqual(rect.id, 2)
-        self.assertEqual(rect.width, 8)
-        self.assertEqual(rect.height, 12)
-        self.assertEqual(rect.x, 4)
-        self.assertEqual(rect.y, 5)
+    def test_update_args(self):
+        """
+        Test for update method: args
+        """
+        R = Rectangle(1, 2, 3, 4, 5)
+        R.update(6)
+        self.assertEqual(6, R.id)
+        R.update(6, 7)
+        self.assertEqual(7, R.width)
+        R.update(6, 7, 8)
+        self.assertEqual(8, R.height)
+        R.update(6, 7, 8, 9)
+        self.assertEqual(9, R.x)
+        R.update(6, 7, 8, 9, 10)
+        self.assertEqual(10, R.y)
 
-        rect.update(width=15, y=7)
-        self.assertEqual(rect.width, 15)
-        self.assertEqual(rect.y, 7)
+    def test_display(self):
+        """
+        Test display method
+        """
+        R = Rectangle(2, 3, 0, 0, 4)
+        with io.StringIO() as bufferIO, redirect_stdout(bufferIO):
+            R.display()
+            output = bufferIO.getvalue()
+            self.assertEqual(output, ('#' * 2 + '\n') * 3)
+        R = Rectangle(2, 3, 4, 5, 6)
+        with io.StringIO() as bufferIO, redirect_stdout(bufferIO):
+            R.display()
+            output = bufferIO.getvalue()
+            self.assertEqual(output,
+                             ('\n' * 5 + (' ' * 4 + '#' * 2 + '\n') * 3))
 
-    def test_to_dictionary(self):
-        rect = Rectangle(5, 10, 2, 3, 1)
-        expected_dict = {'id': 1, 'width': 5, 'height': 10, 'x': 2, 'y': 3}
-        self.assertEqual(rect.to_dictionary(), expected_dict)
+    def test_update_kwargs(self):
+        """
+        Test for update method: kwargs
+        """
+        R = Rectangle(1, 2, 3, 4, 5)
+        R.update(6, id=7)
+        self.assertEqual([R.id, R.width, R.height, R.x, R.y], [6, 1, 2, 3, 4])
+        R.update(6, 7, 8, x=9, y=10)
+        self.assertEqual([R.id, R.width, R.height, R.x, R.y], [6, 7, 8, 3, 4])
+        R.update(width=7, id=6, height=8)
+        self.assertEqual([R.id, R.width, R.height, R.x, R.y], [6, 7, 8, 3, 4])
+        R.update(x=40, y=5)
+        self.assertEqual([R.id, R.width, R.height, R.x, R.y], [6, 7, 8, 40, 5])
 
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_dictionary(self):
+        """
+        Tests for dictionary method
+        """
+        R = Rectangle(100, 200, 300, 400, 500)
+        R_dict = R.to_dictionary()
+        self.assertEqual(R_dict['width'], 100)
+        self.assertEqual(R_dict['height'], 200)
+        self.assertEqual(R_dict['x'], 300)
+        self.assertEqual(R_dict['y'], 400)
+        self.assertEqual(R_dict['id'], 500)
