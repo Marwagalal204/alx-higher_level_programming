41aed8bb66fbefe3d611185cd3bfeb04d5c75dd4
marwagalal204 <marwagalal204@gmail.com>
2024-01-15 20:48:01 +0200

    test unit

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -11,6 +11,8 @@ import io
 import pep8
 from contextlib import redirect_stdout
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
