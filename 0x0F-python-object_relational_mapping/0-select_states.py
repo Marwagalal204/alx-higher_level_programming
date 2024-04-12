20eef11dfd39bae1ed72a78f07658eb57cceb696
Marwwagalal204 <Marwwagalal204@gmail.com>
2024-04-12 08:39:47 +0200

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
@@ -8,10 +7,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user= sys.argv[1],
                          passwd= sys.argv[2], db= sys.argv[3], port= 3306)
     
-mycursor = db.cursor()
-mycursor.execute("SELECT * FROM states")
-rows = mycursor.fetchall()
-for row in rows:
-    print(row)
-mycursor.close()
-db.close()
+    mycursor = db.cursor()
+    mycursor.execute("SELECT * FROM states")
+    rows = mycursor.fetchall()
+    for row in rows:
+        print(row)
+    mycursor.close()
+    db.close()
